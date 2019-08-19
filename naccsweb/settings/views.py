e05741b8ee6ecaf07b6adf091bb0e166b954854a
David Wang <davidw1339@gmail.com>
2019-08-18 20:07:27 -0400

    pulled out faceit auth into function

--- naccsweb/settings/views.py
+++ naccsweb/settings/views.py
@@ -10,6 +10,11 @@ DISCORD_TOKEN_URL = DISCORD_ENDPOINT + '/oauth2/token'
 DISCORD_CLIENT_ID = os.environ.get('DISCORD_CLIENT_ID')
 DISCORD_CLIENT_SECRET = os.environ.get('DISCORD_CLIENT_SECRET')
 
+FACEIT_TOKEN_ENDPOINT = 'https://api.faceit.com/auth/v1/oauth/token'
+FACEIT_INFO_ENDPOINT = 'https://api.faceit.com/auth/v1/resources/userinfo'
+FACEIT_CLIENT_ID = os.environ.get('FACEIT_CLIENT_ID')
+FACEIT_CLIENT_SECRET = os.environ.get('FACEIT_CLIENT_SECRET')
+
 def get_discord_name(code):
     data = {
         'client_id':        DISCORD_CLIENT_ID,
@@ -34,37 +39,38 @@ def get_discord_name(code):
     r = requests.get(DISCORD_ENDPOINT + '/users/@me', headers=headers)
     return r.json()['username']
 
+def get_faceit_name(faceit_code):
+    data = {
+        'code': faceit_code,
+        'grant_type': 'authorization_code'
+    }
+
+    authorization = f'{FACEIT_CLIENT_ID}:{FACEIT_CLIENT_SECRET}'.encode()
+    authorization_enc = base64.b64encode(authorization)
+    headers = {
+        'Authorization': 'Basic ' + authorization_enc.decode()
+    }
+
+    # exchange code for authorization token
+    response = requests.post(FACEIT_TOKEN_ENDPOINT, data=data, headers=headers)
+
+    access_token = response.json().get('access_token')
+    headers = {
+        'Authorization': 'Bearer ' + access_token
+    }
+    response =  requests.get(FACEIT_INFO_ENDPOINT, headers=headers)
+    return response.json().get('nickname')
+
 @login_required
 def account(request):
     return render(request, 'settings/account.html')
 
 @login_required
 def faceit(request):
-    # grab faceit code from URL
     faceit_code = request.GET.get('code')
-    print(faceit_code)
-    # create authorization
-    authorization = b'4c53be11-4f4a-4f71-845a-7a094004f4a0:T6pKFm8vR28qUWyOpe2IdIoksbs3RC5Z2DXuEM4e'
-    authorization_enc = base64.b64encode(authorization)
-    print(authorization_enc.decode())
-    # exchange code for authorization token
-    response = requests.post('https://api.faceit.com/auth/v1/oauth/token', headers={
-        'Authorization': 'Basic ' + authorization_enc.decode()
-    },
-    data={
-        'code': faceit_code,
-        'grant_type': 'authorization_code'
-    })
+    faceit_username = get_faceit_name(faceit_code)
 
-    print(response.json())
-
-    access_token = response.json().get('access_token')
-    headers = {
-        'Authorization': 'Bearer ' + access_token
-    }
-    response =  requests.get('https://api.faceit.com/auth/v1/resources/userinfo', headers=headers)
-    faceit_username = response.json().get('nickname')
-
+    # Enter faceit name into user profile
     user = User.objects.get(username=request.user.username)
     user.profile.faceit = faceit_username
     user.save()
