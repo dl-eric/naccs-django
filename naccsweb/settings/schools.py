5d2d2756f8b2877d3229ffd2c9135f4364a623bb
Eric <ericdl712@gmail.com>
2019-08-29 21:03:07 -0500

    Add domains from duplicated schools

--- naccsweb/settings/schools.py
+++ naccsweb/settings/schools.py
@@ -12,9 +12,17 @@ def get_schools():
     mapping = {}
 
     for school in ca:
-        mapping[school.get('name')] = school.get('domains')
+        # The API we use have duplicates for some reason... so we add domains
+        # from each duplicate.
+        if mapping.get(school.get('name')):
+            mapping[school.get('name')] += school.get('domains')
+        else:
+            mapping[school.get('name')] = school.get('domains')
     
     for school in us:
-        mapping[school.get('name')] = school.get('domains')
+        if mapping.get(school.get('name')):
+            mapping[school.get('name')] += school.get('domains')
+        else:
+            mapping[school.get('name')] = school.get('domains')
 
     return mapping
