b5730fb583f33e5714561dd2b0a1b007efac43f8
Eric <ericdl712@gmail.com>
2019-08-31 15:55:00 -0500

    Fix favicon, remove ')' from confirmation emails

--- naccsweb/settings/urls.py
+++ naccsweb/settings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('verify/<slug:uidb64>/<slug:token>)/', views.verify, name='verify'),
+    path('verify/<slug:uidb64>/<slug:token>/', views.verify, name='verify'),
     path(r'account/pending', views.pending, name='pending'),
     path(r'account', views.account, name='account'),
     path(r'account/faceitconfirm', views.faceit, name='faceitconfirm'),
--- naccsweb/users/urls.py
+++ naccsweb/users/urls.py
@@ -6,7 +6,7 @@ from .decorators import logout_required
 urlpatterns = [
     path(r'users/<page_alias>', views.profile, name='profile'),
     path(r'register/', logout_required(views.register), name='register'),
-    path('activate/<slug:uidb64>/<slug:token>)/', logout_required(views.activate), name='activate'),
+    path('activate/<slug:uidb64>/<slug:token>/', logout_required(views.activate), name='activate'),
     path(r'register/pending', logout_required(views.pending), name='pending_confirmation'),
     path(r'register/confirmed', logout_required(views.confirmed), name='confirmed'),
     path(r'notfound/', logout_required(views.not_found), name='not found')
