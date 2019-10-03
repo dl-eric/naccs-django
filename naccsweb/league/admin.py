6cfd4cfb9ea318387565b7bc3420692a943d58ab
Eric <ericdl712@gmail.com>
2019-10-02 23:11:56 -0500

    Progress

--- naccsweb/league/admin.py
+++ naccsweb/league/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Team, School
 
+class TeamInline(admin.StackedInline):
+    model = Team
+    can_delete = False
+    verbose_name_plural = 'Teams'
+    fk_name = 'school'
+
+class SchoolAdmin(admin.ModelAdmin):
+    inlines = (TeamInline, )
+    list_display = ('name', 'city', 'state', 'is_active')
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(SchoolAdmin, self).get_inline_instances(request, obj)
+
 admin.site.register(Team)
-admin.site.register(School)
+admin.site.register(School, SchoolAdmin)
--- naccsweb/league/models.py
+++ naccsweb/league/models.py
@@ -5,8 +5,11 @@ class School(models.Model):
     name = models.CharField(max_length=80, blank=True)
     city = models.CharField(max_length=64, blank=True)
     state = models.CharField(max_length=64, blank=True)
+    is_active = models.BooleanField(default=False, blank=True)
 
 class Team(models.Model):
     name = models.CharField(max_length=80, blank=True)
+    division = models.CharField(max_length=32, blank=True)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+    captain = models.CharField(max_length=80, blank=True)
     roster = models.ManyToManyField(User)
